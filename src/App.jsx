2ea10eabbccdc7b43d9e40a7ee7461571a378129
Altyn Charyyeva <altyncharyyeva1@gmail.com>
2026-04-07 15:31:44 +0300

    updated the project links

--- src/App.jsx
+++ src/App.jsx
@@ -4,24 +4,35 @@ import "./App.css";
 const projects = [
   {
     id: 1,
-    title: "E-Commerce Dashboard",
-    description: "A real-time sales tracker built with React and Chart.js.",
-    tech: ["React", "Vite", "Tailwind"],
-    link: "#",
+    title: "Energy Management System (Part 1)",
+    description:
+      "A Containerized Microservices Architecture for measuring and monitoring the energy consumption of different devices of users.",
+    tech: ["Spring Boot", "React", "Docker"],
+    link: "https://github.com/AltynCharyyeva/microservices-basics-rest-docker",
   },
   {
     id: 2,
-    title: "AI Image Generator",
-    description: "Interface for generating art using OpenAI's DALL-E API.",
-    tech: ["React", "Node.js", "API"],
-    link: "#",
+    title: "Energy Management System (Part 2)",
+    description:
+      "An event-driven architecture using RabbitMQ as a message broker and Traefik as load balancer and reverse proxy.",
+    tech: ["Spring Boot", "React", "Docker", "RabbitMQ", "Traefik"],
+    link: "https://github.com/AltynCharyyeva/microservices-event-driven-rabbitmq",
   },
   {
     id: 3,
-    title: "Portfolio Website",
-    description: "The very site you are looking at right now!",
-    tech: ["React", "GitHub Pages"],
-    link: "#",
+    title: "E-Ticket App",
+    description:
+      "The E-Ticket App provides users with a convenient platform to manage and access electronic tickets for various events and activities.",
+    tech: ["Django", "HTML & CSS", "JavaScript"],
+    link: "https://github.com/AltynCharyyeva/E-TicketApp",
+  },
+  {
+    id: 4,
+    title: "E-Learning App",
+    description:
+      "The E-Learning App is a robust platform for delivering educational content online, facilitating seamless learning experiences for students and instructors.",
+    tech: ["Spring Boot", "React", "MySQL", "RESTful API"],
+    link: "https://github.com/AltynCharyyeva/E-LearningApp",
   },
 ];
 
@@ -70,7 +81,7 @@ function App() {
                 ))}
               </div>
               <a href={project.link} className="project-link">
-                Live Demo →
+                Link to GitHub →
               </a>
             </div>
           ))}
